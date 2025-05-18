b377e17a362a87cb48c5920ac531c2e89134a499
kofi arhin <kofiarhin69@gmail.com>
2025-05-18 05:56:31 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,21 +1,27 @@
 const express = require("express");
+const cors = require("cors");
+
+const users = require("./config/users");
+const products = require("./config/products");
 
 const app = express();
-const cors = require("cors");
-const users = require("./config/users");
+const port = process.env.PORT || 5000;
 
-// setup middlewares
 app.use(cors());
 app.use(express.json());
 
-app.get("/", async (req, res, next) => {
-  return res.json({ message: "hello world " });
+app.get("/", (req, res) => {
+  res.json({ message: "hello world" });
 });
 
-app.get("/api/users", async (req, res, next) => {
-  return res.json(users);
+app.get("/api/users", (req, res) => {
+  res.json(users);
 });
 
-const port = process.env.PORT || 5000;
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
 
-app.listen(port, () => console.log("server started on port ", port));
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
